5a697155853c81b5e3c9a4179c4df8833a53b948
petrovic-ivan <ivan.petrovic.srb@outlook.com>
2019-03-05 21:23:33 -0800

    Added envs in docker-compose and logging.

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ pgClient.on('error', () => console.log('Lost PG connection'));
 
 pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+  .catch(err => console.log('CONNECTION ERROR: ', err));
 
 // Redis Client Setup
 const redis = require('redis');
